9f2299aa6398a5fedc42bb237deaef05d11cba0a
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-28 21:16:22 +0900

    state사용

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,13 +6,17 @@ import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
+  let [글제목, b] = useState("남자 코트 추천");
 
   return (
     <div className="App">
       <div class="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <div>{post}</div>
+      <div className="list">
+        <h4>{글제목}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
