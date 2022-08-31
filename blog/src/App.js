dc986cf3195e22c948a8fcbe4513ad4417241fee
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-31 22:45:42 +0900

    onClick내 함수 사용

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,6 +8,9 @@ function App() {
   let post = "강남 우동 맛집";
   let [글제목, b] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
   let [따봉, 따봉변경] = useState(0);
+  function 따봉변경함수() {
+    따봉변경(따봉 + 1);
+  }
 
   return (
     <div className="App">
@@ -16,15 +19,7 @@ function App() {
       </div>
       <div className="list">
         <h4>
-          {글제목[0]}{" "}
-          <span
-            onClick={() => {
-              따봉변경(따봉 + 1);
-            }}
-          >
-            👍
-          </span>{" "}
-          {따봉}
+          {글제목[0]} <span onClick={따봉변경함수}>👍</span> {따봉}
         </h4>
         <p>2월 17일 발행</p>
       </div>
