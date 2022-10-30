3981ad1031053e4ed7ac70a27eb2897012994dec
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-10-31 00:42:31 +0900

    이미지넣기, pubilc폴더 사용

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,22 +1,42 @@
 import { Button, Navbar, Container, Nav } from 'react-bootstrap'
 import logo from './logo.svg';
+import bg from './img/bg.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="light" variant="light">
           <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand href="#home">ShoesMarket</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#features">Features</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
             </Nav>
           </Container>
-      </Navbar>s
-    </div>
+      </Navbar>
 
+      <div className='main-bg' style={{backgroundImage: 'url('+bg+')'}}></div>
+      <div className='main-bg'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className='col-md-4'> <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className='col-md-4'> <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
   );
 }
 
